9066ae31e01d9736b328a8b512a78685905ec61a
Dominik Moritz <domoritz@gmail.com>
2012-10-01 13:21:15 +0100

    Added some install info.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.1.1'
 
 setup(
     name='ckanext-htsql',
     version=version,
-    description="Enables HTSQL queries in the new datastore.",
+    description="Enables HTSQL queries in the new datastore in CKAN.",
     long_description="""\
+    The new datastore offers a way to store data directly in CKAN.
+    This plugin adds an API enpoint 'datastore_search_htsql' that allows htsql
+    on the datastore. Because of restrictions with the sql endpoint, not all htsql queries
+    are possible.
     """,
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='ckan, datastore, htsql',
     author='Dominik Moritz',
     author_email='',
     url='',
-    license='',
+    license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.htsql'],
     include_package_data=True,
